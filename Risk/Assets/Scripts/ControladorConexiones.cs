f970e2a6ff9166a09bc3a673db4b8b2280f0e98c
R3N <r3nstark@gmail.com>
2021-03-20 04:19:04 +0100

    Implementadas funciones básicas para realizar peticiones HTTP e inicio de sesión / registro

---
+++ Risk/Assets/Scripts/ControladorConexiones.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ControladorConexiones : MonoBehaviour
+{
+	private const float MAX_TIME = 10; // Tiempo máximo en segundos que puede tardar una conexión
+	private const string DIRECCION_PETICIONES = "https://fathomless-ridge-74437.herokuapp.com/";
+	public static ControladorConexiones instance; // Referencia estática a si mismo para usar como singleton
+	
+	private void Awake() {
+		// Es necesario utilizar un singleton porque no se puede invocar a un corrutina desde una clase estática
+		instance = this;
+	}
+	
+	// Devuelve el string recibido tras hacer una petición HTTP con el formulario
+	public async Task<string> RequestHTTP(string direccion, WWWForm form){
+		string temp = null;
+		// Realizamos la petición HTTP a traves de la corrutina
+		StartCoroutine(SendRequest<string>(direccion, form, (returned) => {
+			temp = returned;
+		}));
+		// Esperamos a recibir la respuesta de la corrutina
+		float totalTime = 0;
+		while(temp == null){
+			await Task.Delay(500);
+			totalTime += 0.5f;
+			if(totalTime >= MAX_TIME){
+				// Se ha esperado el tiempo máximo, devolver error
+				temp = "ERROR";
+				break;
+			}
+		}
+		return temp;
+	}
+	
+	// Envia una petición HTTP con el formulario y devuelve el resultado en texto a la función de callback
+	public IEnumerator SendRequest<T>(string direccion, WWWForm form, Action<T> callback) {
+		UnityWebRequest www = UnityWebRequest.Post(DIRECCION_PETICIONES + direccion, form);
+		yield return www.SendWebRequest();
+		if(www.isNetworkError || www.isHttpError) {
+			print("Error de conexión HTTP");
+		} else {
+			// Convertimos el texto recibido a tipo generico para poder devolverselo al callback
+			T t = (T)Convert.ChangeType(www.downloadHandler.text, typeof(T));
+			if(callback != null)
+			{
+				callback(t);
+			}
+
+		}
+	}
+}
---
+++ Risk/Assets/Scripts/ControladorSesion.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ControladorSesion : MonoBehaviour
+{
+	private string usuario, clave, correo;
+	private bool recibeCorreos;
+	public void ActualizarUsuario(string nuevoUsuario)
+	{
+		usuario = nuevoUsuario;
+	}
+	
+	public void ActualizarClave(string nuevaClave)
+	{
+		clave = nuevaClave;
+	}
+	
+	public void ActualizarCorreo(string nuevoCorreo)
+	{
+		correo = nuevoCorreo;
+	}
+	
+	public void ActualizarRecibeCorreo(bool nuevoRecibir){
+		recibeCorreos = nuevoRecibir;
+	}
+	
+	public async void IniciarSesion()
+	{
+		WWWForm form = new WWWForm();
+		form.AddField("usuario", usuario);
+		form.AddField("clave", clave);
+		string result = await ControladorConexiones.instance.RequestHTTP("iniciarSesion",form);
+		// TODO: Mover a pantalla principal si exito o a pantalla de error si ha dado error
+		print("Recibido de RequestHTTP: " + result);
+	}
+	
+	public async void Registrarse()
+	{
+		WWWForm form = new WWWForm();
+		form.AddField("usuario", usuario);
+		form.AddField("correo", correo);
+		form.AddField("clave", clave);
+		form.AddField("recibeCorreos", recibeCorreos.ToString());
+		string result = await ControladorConexiones.instance.RequestHTTP("registrar", form);
+		// TODO: Mover a pantalla principal si exito o a pantalla de error si ha dado error
+		print("Recibido de RequestHTTP: " + result);
+	}
+}
