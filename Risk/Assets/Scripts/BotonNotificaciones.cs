21807fa84823341977502f6e4fd1808bb2d0bce7
Niako <pkmniako@gmail.com>
2021-05-09 00:33:43 +0200

    Mejora de obtencion de notificaciones en la pantalla principal

--- Risk/Assets/Scripts/BotonNotificaciones.cs
+++ Risk/Assets/Scripts/BotonNotificaciones.cs
@@ -22,6 +22,8 @@ public class BotonNotificaciones : MonoBehaviour
     private GameObject indicadorNotificaciones; //Imagen que indica si hay notificaciones y sirve de fondo para el numero de estas
 
     private const int tiempoRefresh = 5000; //Tiempo en milisegundos entre re-peticiones de obtencion de notificaciones
+	
+	private bool descargandoNotificaciones = false; //Indica si se estan descargando notificaciones o no
 
     private void OnEnable() {
 		//Re-iniciar los boton
@@ -29,41 +31,53 @@ public class BotonNotificaciones : MonoBehaviour
 		numeroNotificaciones.text = "LD";
 		indicadorNotificaciones.SetActive(false);
         iconoCargando.SetActive(true);
+	
 
-		ActualizarNotificaciones();
+		//Iniciar el proceso de actualizar notificaciones si no se esta realizando ya
+		if(!descargandoNotificaciones)
+			ActualizarNotificaciones();
+		else
+			ActualizarInterfaz();
     }
 
 	//Actualiza las notificaciones en el controlador 
 	private async void ActualizarNotificaciones() {
         //Repetir obtencion de notificaciones mientras el boton de notificaciones este en pantalla
         //Repetir cada tiempoRefresh segundos
+		descargandoNotificaciones = true;
+
 		while(this != null && this.gameObject.activeInHierarchy) {
-            //Debug.Log("Obteniendo notificaciones...");
 
             //Obtener notificaciones
 			await ControladorNotificaciones.ObtenerNotificaciones(false);
 
-			//Obtener numero de notificaciones
-			int n = 0;
-			if(ControladorNotificaciones.notificaciones != null)
-				n = ControladorNotificaciones.notificaciones.Count;
-
-			//Actualizar los datos en pantalla, permitirle dar al boton
-			indicadorNotificaciones.SetActive(true);
-			botonNotificacion.interactable = true;
-            iconoCargando.SetActive(false);
-
-			if(n < 0)
-				numeroNotificaciones.text = "ER";
-			else if(n == 0)
-				indicadorNotificaciones.SetActive(false);
-			else if(n > 9)
-				numeroNotificaciones.text = "9+";
-			else
-				numeroNotificaciones.text = n.ToString();
+			//Actualizar Interfaz
+			ActualizarInterfaz();
 
 			//Scheudle esta funcion para dentro de 10 segundos
             await Task.Delay(tiempoRefresh);
 		}
+		descargandoNotificaciones = false;
+	}
+
+	// Actualiza exclusivamente la interfaz del boton (Numero de notificaciones)
+	private void ActualizarInterfaz() {
+		int n = 0;
+		if(ControladorNotificaciones.notificaciones != null)
+			n = ControladorNotificaciones.notificaciones.Count;
+
+		//Actualizar los datos en pantalla, permitirle dar al boton
+		indicadorNotificaciones.SetActive(true);
+		botonNotificacion.interactable = true;
+        iconoCargando.SetActive(false);
+
+		if(n < 0)
+			numeroNotificaciones.text = "ER";
+		else if(n == 0)
+			indicadorNotificaciones.SetActive(false);
+		else if(n > 9)
+			numeroNotificaciones.text = "9+";
+		else
+			numeroNotificaciones.text = n.ToString();
 	}
 }
